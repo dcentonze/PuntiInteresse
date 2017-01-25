93c8582ab64716edc25ec557aa8ce417a7c60f07
Angelo Cociorva <Angelo Cociorva>
2017-01-25 13:10:25 +0100

    EditText and TextView inserted

--- mobile/src/main/java/android/vm/puntiinteresse/MainActivity.java
+++ mobile/src/main/java/android/vm/puntiinteresse/MainActivity.java
@@ -2,6 +2,10 @@ package android.vm.puntiinteresse;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by acbes on 25/01/2017.
@@ -9,10 +13,23 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+    TextView welcomeTv;
+    Button changeBt;
+    EditText changeTextEdit;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        welcomeTv=(TextView)findViewById(R.id.welcome_tv);
+        changeBt=(Button)findViewById(R.id.change_bt);
+        changeTextEdit=(EditText)findViewById(R.id.edit_txt);
+        changeBt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                welcomeTv.setText(changeTextEdit.getText());
+            }
+        });
+
     }
 
 
