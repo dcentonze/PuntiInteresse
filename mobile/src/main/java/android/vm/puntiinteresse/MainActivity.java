51686ea6ca07dc33f009336abc106cdbc346aac3
Angelo Cociorva <Angelo Cociorva>
2017-01-25 11:30:33 +0100

    Created first activity

---
+++ mobile/src/main/java/android/vm/puntiinteresse/MainActivity.java
@@ -0,0 +1,20 @@
+package android.vm.puntiinteresse;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by acbes on 25/01/2017.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
